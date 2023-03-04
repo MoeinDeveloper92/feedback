3ce2c0b45443f46789836a044699347cd6e90592
MoeinDeveloper92 <moeinsamani1992@gmail.com>
2023-03-04 19:13:37 +0330

    adding them btn to the cards

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-function Card({ children, reverse }) {
+function Card({ children }) {
+    const [reverse, setReverse] = useState(true)
+
+    const handleclick = (e) => {
+        setReverse(pre => !pre)
+    }
     return (
         <div className={`card ${reverse && 'reverse'}`}>
             {children}
+            <button className={`btn-${reverse ? 'secondary' : 'primary'}`} onClick={handleclick}>Toggle Theme</button>
         </div>
     )
 }
 
 
 
-Card.defaultProps = {
-    reverse: false
-}
 
 
 Card.propTypes = {
     children: PropTypes.node.isRequired,
-    reverse: PropTypes.bool
+
 }
 export default Card
